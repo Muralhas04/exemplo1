fc59b280a380325d1fa6267b562fda68772896d4
Muralhas04 <luisfaiscacandeias@gmail.com>
2026-05-28 12:38:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 print("olá mundo!")
 nome = input("Digite o seu nome:")
 print(f"Bem-vindo, {nome}!")
+
+print("Vamos fazer uma conta de soma.")
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+soma = num1 + num2
+
+print(f"A soma de {num1} e {num2} é: {soma}")
